87846edcf5e4746968c413f684a7204da6bcdfe4
Masaxxx6102 <mm0217snowflake@gmail.com>
2026-03-06 00:13:29 +0900

    Add files via upload

--- src/chart_generator.py
+++ src/chart_generator.py
@@ -85,7 +85,8 @@ class ChartGenerator:
         
         # Prepare style with market colors and explicit Japanese font family
         mc = mpf.make_marketcolors(up='#ff4444', down='#00c853', edge='inherit', wick='inherit', volume='in', inherit=True)
-        s = mpf.make_mpf_style(base_mpf_style='night', marketcolors=mc, gridstyle='--', 
+        # 'night' is not always available, using 'charles' as base and overriding to deep dark
+        s = mpf.make_mpf_style(base_mpf_style='charles', marketcolors=mc, gridstyle='--', 
                              y_on_right=False, rc={'font.family': 'IPAexGothic', 'axes.facecolor': '#000000', 'figure.facecolor': '#000000', 'text.color': 'white', 'axes.labelcolor': 'white', 'xtick.color': 'white', 'ytick.color': 'white', 'grid.color': '#1f1f1f'})
         
         # Prepare figure (Removed invalid 'fontfamily' kwarg)
